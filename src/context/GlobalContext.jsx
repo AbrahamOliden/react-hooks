fca810f6e0402fd94f5fe4512361256f6cb70786
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-14 19:39:13 -0600

    add all states removed from App

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -7,6 +7,8 @@ import React from 'react'
 const GlobalContextProvider = ({children}) => {
     
     const [search, setSearch] = useState('');
+    const [fotosDeGaleria, setFotosDeGaleria] = useState([]);
+    const [fotoSeleccionada, setFotoSeleccionada] = useState(null);
 
     return (
         <GlobalContext.Provider value={{ search, setSearch } } >
