06feaa4b9e12636d0762cac6a5afe41d0316f390
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-14 18:08:08 -0600

    create Cargando component

---
+++ src/components/Cargando/index.jsx
@@ -0,0 +1,12 @@
+function Cargando() {
+    return (
+        <div style={{display: 'flex', justifyContent: 'center'}} >
+            <img 
+                src="/img/loading.gif" 
+                alt="loading animation" 
+                style={{width: '30vw'}}/>
+        </div>
+    )
+};
+
+export default Cargando;
