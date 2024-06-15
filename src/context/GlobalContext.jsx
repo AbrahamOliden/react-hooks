c859a964a18b5ab1f19ef740bf3be09a939bb813
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-14 19:41:50 -0600

    add alAlternarFavorito from App

--- src/context/GlobalContext.jsx
+++ src/context/GlobalContext.jsx
@@ -10,6 +10,24 @@ const GlobalContextProvider = ({children}) => {
     const [fotosDeGaleria, setFotosDeGaleria] = useState([]);
     const [fotoSeleccionada, setFotoSeleccionada] = useState(null);
 
+    const alAlternarFavorito = (foto) => {
+
+        if (foto.id === fotoSeleccionada?.id) {
+          setFotoSeleccionada({
+            ...fotoSeleccionada,
+            favorita: !fotoSeleccionada.favorita
+          })
+    
+        }
+    
+        setFotosDeGaleria(fotosDeGaleria.map(fotoDeGaleria => {
+          return {
+            ...fotoDeGaleria,
+            favorita: fotoDeGaleria.id === foto.id ? !foto.favorita : fotoDeGaleria.favorita
+          }
+        }))
+      }
+
     return (
         <GlobalContext.Provider value={{ search, setSearch } } >
             {children}
