b189b4c42d53d562f871b836f3be2765c92ffeeb
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-17 02:03:31 -0600

    remove setSearch prop

--- src/components/Cabecera/index.jsx
+++ src/components/Cabecera/index.jsx
@@ -1,8 +1,5 @@
 import styled from "styled-components"
 import CampoTexto from "../CampoTexto"
-import { useContext } from "react"
-import { GlobalContext } from "../../context/GlobalContext"
-
 
 const HeaderEstilizado = styled.header`
    padding: 60px 0;
@@ -15,11 +12,9 @@ const HeaderEstilizado = styled.header`
 
 const Cabecera = () => {
 
-    const { setSearch } = useContext(GlobalContext)
-
     return <HeaderEstilizado>
         <img src="img/logo.png" alt="Logo de Space App" />
-        <CampoTexto setSearch={setSearch} />
+        <CampoTexto />
     </HeaderEstilizado>
 }
 
