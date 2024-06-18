35d4a005820728621da556a8a6ef091047e89fc4
AbrahamOliden <abrahamoliden@protonmail.com>
2024-06-17 18:21:51 -0600

    create useModalPhoto, its functions and consts

---
+++ src/hooks/useFotoModal.jsx
@@ -0,0 +1,22 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
+
+function useModalPhoto() {
+    const { state, dispatch } = useContext(GlobalContext);
+
+    const openModal = (foto) => {
+        dispatch({ type: 'SET_FOTO_SELECCIONADA', payload: foto})
+    };
+
+    const closeModal = (foto) => {
+        dispaatch({ type: 'SET_FOTO_SELECCIONADA', payload: null})
+    };
+
+    const selectedPhoto = state.fotoSeleccionada;
+
+    const isModalOpen = state.modalAbierto;
+
+    return { selectedPhoto, isModalOpen, openModal, closeModal };
+};
+
+export default useModalPhoto;
